64c7a38d848193fcd209e3a1999737a9e5fb11d2
Gustavo Zimbron <pptavozl@gmail.com>
2023-05-15 13:32:25 -0600

    Adding CoolButton and it tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import CoolButton from '$lib/components/CoolButton.svelte';
+
+	const onClick = (e: CustomEvent<MouseEvent>) => {
+		console.log('click', e);
+	};
+</script>
+
+<h1>CI/CD Test Package</h1>
+<p>
+	This package was created to test ci/cd using github actions with Sveltekit package repository.
+</p>
+<h2 style:color="red">¡THIS PACKAGE IS FOR TEST!</h2>
+
+<div class="viewer">
+	<CoolButton on:click={onClick}>
+		<div class="buttonIndex">
+			<p>😎</p>
+			<p>Hi!</p>
+		</div>
+	</CoolButton>
+</div>
+
+<style>
+	.viewer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 50px;
+	}
+	.buttonIndex {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 5px;
+	}
+
+	.buttonIndex p {
+		margin: 0;
+	}
+</style>
